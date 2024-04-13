ec33b13da8eb7791f0681476eef474d94546cf58
cherlton <165408934+cherlton@users.noreply.github.com>
2024-04-13 02:57:25 +0200

    Create Module.java

---
+++ Module.java
@@ -0,0 +1,40 @@
+package za.ac.tut.entity;
+/**
+*
+* @author MemaniV
+*/
+public class Module {
+ private Integer id;
+ private String name;
+ private String code;
+ public Module() {
+ }
+7
+ public Module(Integer id, String name, String code) {
+ this.id = id;
+ this.name = name;
+ this.code = code;
+ }
+ public Integer getId() {
+ return id;
+ }
+ public void setId(Integer id) {
+ this.id = id;
+ }
+ public String getName() {
+ return name;
+ }
+ public void setName(String name) {
+ this.name = name;
+ }
+ public String getCode() {
+ return code;
+ }
+ public void setCode(String code) {
+ this.code = code;
+ }
+ @Override
+ public String toString() {
+ return id + "," + name + "," + code;
+ }
+}
